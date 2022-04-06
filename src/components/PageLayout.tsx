39cb5151ed82b5863922352c0daab89087bed584
Daniel Robinson <521946+DanielRobinsonSoftware@users.noreply.github.com>
2022-04-06 21:52:51 +1200

    Revert "Merge pull request #12 from DanielRobinsonSoftware/install-emotion"
    This reverts commit fde53a6d0cb8e04f49d45a3fa339039be370ed1c, reversing
    changes made to d399b5328889d6670684253bca29b49c9c3498fd.

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,15 +1,29 @@
-/** @jsxImportSource @emotion/react */
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { useMsal, useIsAuthenticated as userIsAuthenticated /*msal library has a typo*/ } from "@azure/msal-react";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
 import { SignUpButton } from "./SignUpButton";
 import Emoji from 'a11y-react-emoji';
-import { css } from '@emotion/react'
 
-const style = css`
-    color: black;
-`
+export const PageLayout = (props: { children: JSX.Element[]; }) => {
+    const isAuthenticated = userIsAuthenticated();
+    return (
+        <>
+            <Navbar bg="primary" variant="dark">
+                <a className="navbar-brand" href="/">
+                    Movie Match
+                </a>
+                <SignInLayout isAuthenticated={isAuthenticated} />
+            </Navbar>
+            <h5>Find a movie to watch together <Emoji symbol="📽️" label="projector" /><Emoji symbol="❤️" label="heart" /></h5>
+            <br />
+            <br />
+            {props.children}
+        </>
+    );
+};
+
 const SignInLayout = (props: { isAuthenticated: boolean; }) => {
     const { accounts } = useMsal();
     const name = accounts[0] && accounts[0].username;
@@ -26,25 +40,6 @@ const SignInLayout = (props: { isAuthenticated: boolean; }) => {
         <>
             {signIn}
             {signUp}
-        </>
-    );
-};
-
-export const PageLayout = (props: { children: JSX.Element[]; }) => {
-
-    const isAuthenticated = userIsAuthenticated();
-    return (
-        <>
-            <Navbar bg="primary" variant="dark">
-                <a className="navbar-brand" href="/">
-                    Movie Match
-                </a>
-                <SignInLayout isAuthenticated={isAuthenticated} />
-            </Navbar>
-            <h5 css={style}>Find a movie to watch together <Emoji symbol="📽️" label="projector" /><Emoji symbol="❤️" label="heart" /></h5>
-            <br />
-            <br />
-            {props.children}
         </>
     );
 };
