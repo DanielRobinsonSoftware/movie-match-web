7f59520b1fad7928a135541cc29be09a7ad20ed1
Daniel Robinson <521946+DanielRobinsonSoftware@users.noreply.github.com>
2022-05-04 23:47:04 +1200

    Split header layout into component. Style header with logo on the left and buttons on the right.

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { useIsAuthenticated as userIsAuthenticated /*msal library has a typo*/ } from "@azure/msal-react";
 import Emoji from 'a11y-react-emoji';
 import { ThemeWrapper } from './ThemeWrapper';
-import { ThemeButton } from './ThemeButton';
 import { useStorage } from '../useStorage';
 import { defaultTheme, ThemeType } from '../theme/theme';
-import { SignInLayout } from './SignInLayout';
+import { HeaderLayout } from './HeaderLayout';
 
 export const PageLayout = (props: { children: JSX.Element[]; }) => {
     let [theme, setTheme] = useStorage<ThemeType>('selectedTheme', defaultTheme);
-    const isAuthenticated = userIsAuthenticated();
     return (
         <ThemeWrapper theme={theme}>
             <div className="App">
-                <span>
-                    <a href="/"><img src="/MovieMatchLogo-118x94.png" alt="Movie Match" /></a>
-                    <SignInLayout theme={theme} isAuthenticated={isAuthenticated} />
-                    <ThemeButton selectedTheme={theme} setTheme={setTheme} />
-                </span>
+                <HeaderLayout theme={theme} setTheme={setTheme}></HeaderLayout>
                 <h5>Find a movie to watch together <Emoji symbol="📽️" label="projector" /><Emoji symbol="❤️" label="heart" /></h5>
                 <br />
                 <br />
--- src/components/SignInLayout.tsx
+++ src/components/SignInLayout.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useIsAuthenticated as userIsAuthenticated /*msal library has a typo*/ } from "@azure/msal-react";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
 import { SignUpButton } from "./SignUpButton";
 import { useMsal } from "@azure/msal-react";
 import { ThemeType } from '../theme/theme';
 
-export const SignInLayout = (props: { theme: ThemeType, isAuthenticated: boolean; }) => {
+export const SignInLayout = (props: { theme: ThemeType }) => {
     const { accounts } = useMsal();
     const name = accounts[0] && accounts[0].username;
-    const signIn = props.isAuthenticated ? (
+    const isAuthenticated = userIsAuthenticated();
+    const signIn = isAuthenticated ? (
         <>
             <h5 className="card-title">Signed in as {name}</h5>
             <SignOutButton theme={props.theme} />
@@ -16,7 +18,7 @@ export const SignInLayout = (props: { theme: ThemeType, isAuthenticated: boolean
     ) : (
         <SignInButton theme={props.theme} />
     );
-    const signUp = !props.isAuthenticated && <SignUpButton theme={props.theme} />;
+    const signUp = !isAuthenticated && <SignUpButton theme={props.theme} />;
     return (
         <>
             {signIn}
